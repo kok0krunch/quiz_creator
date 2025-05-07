a247460802243cd0a55a87c480fde4c9998fc0ea
kok0krunch <amaliakadoi@gmail.com>
2025-05-07 22:54:36 +0800

    Define a function to load quiz question from the file and check if the file exist

--- quiz_creator.py
+++ quiz_creator.py
@@ -5,3 +5,10 @@ import random
 
 # File path for the quiz data question
 quiz_question = "quiz_questions.txt"
+
+# Load the quiz questions from the file
+def load_quiz_questions():
+    # Check if the quiz question file exists
+    if not os.path.exists(quiz_question):
+        print(Fore.RED + "❌ Error: The file {quiz_question} does not exist." + Style.RESET_ALL)
+        sys.exit(1)
